f28f10e34013b9dd93debf26b978ef35b680f3fe
dunndealpro <dunn.evan.f@gmail.com>
2023-03-21 14:32:59 -0700

    update background color

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -6,13 +6,13 @@ import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 
 export default function LandingPage(props) {
-  console.log("landing page! ", { props }) 
+  console.log("landing page! ", { props })
 
 
   // const detectKeyPress = (e)=>{
   //   console.log("Yo dawg, you pressed ",  e.key)
   //   console.log("ENTRY COUNT: ", props.entryCount)
-    
+
   //   if (props.entryCount < 6) {
   //     console.log("ENTRY COUNT: ", props.entryCount)
   //     let key = e.key
@@ -58,12 +58,12 @@ export default function LandingPage(props) {
   //             console.log(props.guess6)
   //         }
   //     }
-      
+
   // } else {
   //     console.log("no more entries")
   // }
 
-  
+
   // }
 
   // console.log("ENTRY COUNT: ", props.entryCount)
@@ -98,82 +98,84 @@ export default function LandingPage(props) {
   //   }
 
   // }
- 
+
 
   return (
     <>
+      <div style={{'background': "RGB(25,35,25,1)", "height": '100vh'}}>
 
-      <GameBoardContainer
-        guess1={props.guess1}
-        guess2={props.guess2}
-        guess3={props.guess3}
-        guess4={props.guess4}
-        guess5={props.guess5}
-        guess6={props.guess6}
-        setGuess1={props.setGuess1}
-        setGuess2={props.setGuess2}
-        setGuess3={props.setGuess3}
-        setGuess4={props.setGuess4}
-        setGuess5={props.setGuess5}
-        setGuess6={props.setGuess6}
-        compareEntry={props.compareEntry}
-        guessInit={props.guessInit}
-        entryCount={props.entryCount}
-        setEntryCount={props.setEntryCount}
-        background={props.background}
-        guess1bg={props.guess1bg}
-        guess2bg={props.guess2bg}
-        guess3bg={props.guess3bg}
-        guess4bg={props.guess4bg}
-        guess5bg={props.guess5bg}
-        guess6bg={props.guess6bg}
-        currentGuessCount={props.currentGuessCount}
-        setCurrentGuessCount={props.setCurrentGuessCount}
-        checkIfWord = {props.checkIfWord}
-        isWord={props.isWord}
-        setIsWord={props.setIsWord}
-      />
+        <GameBoardContainer
+          guess1={props.guess1}
+          guess2={props.guess2}
+          guess3={props.guess3}
+          guess4={props.guess4}
+          guess5={props.guess5}
+          guess6={props.guess6}
+          setGuess1={props.setGuess1}
+          setGuess2={props.setGuess2}
+          setGuess3={props.setGuess3}
+          setGuess4={props.setGuess4}
+          setGuess5={props.setGuess5}
+          setGuess6={props.setGuess6}
+          compareEntry={props.compareEntry}
+          guessInit={props.guessInit}
+          entryCount={props.entryCount}
+          setEntryCount={props.setEntryCount}
+          background={props.background}
+          guess1bg={props.guess1bg}
+          guess2bg={props.guess2bg}
+          guess3bg={props.guess3bg}
+          guess4bg={props.guess4bg}
+          guess5bg={props.guess5bg}
+          guess6bg={props.guess6bg}
+          currentGuessCount={props.currentGuessCount}
+          setCurrentGuessCount={props.setCurrentGuessCount}
+          checkIfWord={props.checkIfWord}
+          isWord={props.isWord}
+          setIsWord={props.setIsWord}
+        />
 
 
-      <KeyBoardContainer
-        currentGuess={props.currentGuess}
-        setCurrentGuess={props.setCurrentGuess}
-        // guessInit={guessInit}
-        guess1={props.guess1}
-        setGuess1={props.setGuess1}
-        guess2={props.guess2}
-        setGuess2={props.setGuess2}
-        guess3={props.guess3}
-        setGuess3={props.setGuess3}
-        guess4={props.guess4}
-        setGuess4={props.setGuess4}
-        guess5={props.guess5}
-        setGuess5={props.setGuess5}
-        guess6={props.guess6}
-        setGuess6={props.setGuess6}
-        // guess1={guess1}
-        // guess2={guess2}
-        // guess3={guess3}
-        // guess4={guess4}
-        // guess5={guess5}
-        // guess6={guess6}
+        <KeyBoardContainer
+          currentGuess={props.currentGuess}
+          setCurrentGuess={props.setCurrentGuess}
+          // guessInit={guessInit}
+          guess1={props.guess1}
+          setGuess1={props.setGuess1}
+          guess2={props.guess2}
+          setGuess2={props.setGuess2}
+          guess3={props.guess3}
+          setGuess3={props.setGuess3}
+          guess4={props.guess4}
+          setGuess4={props.setGuess4}
+          guess5={props.guess5}
+          setGuess5={props.setGuess5}
+          guess6={props.guess6}
+          setGuess6={props.setGuess6}
+          // guess1={guess1}
+          // guess2={guess2}
+          // guess3={guess3}
+          // guess4={guess4}
+          // guess5={guess5}
+          // guess6={guess6}
 
-        // setGuess1={setGuess1}
-        // setGuess2={setGuess2}
-        // setGuess3={setGuess3}
-        // setGuess4={setGuess4}
-        // setGuess5={setGuess5}
-        // setGuess6={setGuess6}
-        compareEntry={props.compareEntry}
-        guessInit={props.guessInit}
-        entryCount={props.entryCount}
-        setEntryCount={props.setEntryCount}
-        currentGuessCount={props.currentGuessCount}
-        setCurrentGuessCount={props.setCurrentGuessCount}
-        checkIfWord = {props.checkIfWord}
-        isWord={props.isWord}
-        setIsWord={props.setIsWord}
-      />
+          // setGuess1={setGuess1}
+          // setGuess2={setGuess2}
+          // setGuess3={setGuess3}
+          // setGuess4={setGuess4}
+          // setGuess5={setGuess5}
+          // setGuess6={setGuess6}
+          compareEntry={props.compareEntry}
+          guessInit={props.guessInit}
+          entryCount={props.entryCount}
+          setEntryCount={props.setEntryCount}
+          currentGuessCount={props.currentGuessCount}
+          setCurrentGuessCount={props.setCurrentGuessCount}
+          checkIfWord={props.checkIfWord}
+          isWord={props.isWord}
+          setIsWord={props.setIsWord}
+        />
+      </div>
 
 
     </>
