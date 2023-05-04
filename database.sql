a600f8235c45dfd150abd6c5b762e395fac95417
dunndealpro <dunn.evan.f@gmail.com>
2023-05-04 14:57:21 -0700

    update guess distribuiton comp

--- database.sql
+++ database.sql
@@ -5,3 +5,23 @@ CREATE TABLE testgame(
     description VARCHAR(255)
 )
 
+
+SELECT 
+"userName", 
+SUM("score")::DECIMAL as total_score,
+COUNT('id')::DECIMAL as total_games,
+ROUND((SUM("score")::DECIMAL)/(COUNT('id')::DECIMAL),3) as ratio
+FROM "Games" group by "userName" order by ratio DESC; 
+
+
+WITH players as (
+    SELECT "userName", "score"
+    FROM "Games"
+    GROUP BY "userName", "score"
+)
+
+SELECT 
+    -- SUM(score) as "total_score",
+    -- (SELECT COUNT('id') as total_games from "Games")
+    ROUND(CAST (float8 (SUM(score)*1000/ (SELECT COUNT('id') as total_games from "Games"))as numeric),4) as "percent"
+    FROM players;
