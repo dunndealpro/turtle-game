d1b1ae47785dee81d37524d2da7926bfbfc22168
dunndealpro <dunn.evan.f@gmail.com>
2023-05-11 20:13:05 -0700

    update to footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,8 +21,8 @@ export default function Footer({ user, setUser }) {
   // let title = "Welcome " + user.name + "!"
 
   return (
-    <div className="fs-5" >
-      <Navbar className="p-2 nav-bg justify-content-center" id="footer" sticky="bot" variant="light">
+    <div className="fs-6" >
+      <Navbar className="p-1 nav-bg justify-content-center" id="footer" sticky="bot" variant="light">
         {/* <Navbar.Brand className="" href="/">
           TURTLE
         </Navbar.Brand> */}
@@ -33,12 +33,12 @@ export default function Footer({ user, setUser }) {
             <Nav.Link activeClassName="active turtle-nav-item" className='turtle-nav-item' href="/about">About</Nav.Link>
             <Nav.Link activeClassName="active" turtle-nav-item   className="turtle-nav-item" href="/score-board">ScoreBoard</Nav.Link> */}
           {/* <Col className='m-2'> */}
-            <a className='d-md-none m-2' href="https://www.linkedin.com/in/dunnevan/" target="blank"><BsLinkedin size={35} className="" /></a>
-            <a className='d-md-none m-2' href="https://github.com/dunndealpro" target="blank"><BsGithub size={35} className="" /></a>
-            <a className='d-md-none m-2' href="mailto:dunn.evan.f@gmail.com?subject=I would love to connect!&body=Great website and portfolio, let's chat!" target="blank"><MdOutlineEmail size={35} className="" /></a>
-            <a style={{ color: 'inherit' }} className='m-2 d-none d-md-inline' href="https://www.linkedin.com/in/dunnevan/" target="blank">LinkedIn</a>
-            <a style={{ color: 'inherit' }} className='m-2 d-none d-md-inline' href="https://github.com/dunndealpro/turtle-game" target="blank">GitHub</a>
-            <a style={{ color: 'inherit' }} className='m-2 d-none d-md-inline' href="mailto:dunn.evan.f@gmail.com?subject=Cool App!&body=Cool app, let's chat!" target="blank">Contact</a>
+            <a className='d-md-none ms-2 me-2' style={{ color: 'rgba(0,0,0,1' }} href="https://www.linkedin.com/in/dunnevan/" target="blank"><BsLinkedin size={25} className="" /></a>
+            <a className='d-md-none ms-2 me-2' style={{ color: 'rgba(0,0,0,1' }}href="https://github.com/dunndealpro" target="blank"><BsGithub size={25} className="" /></a>
+            <a className='d-md-none ms-2 me-2' style={{ color: 'rgba(0,0,0,1' }} href="mailto:dunn.evan.f@gmail.com?subject=I would love to connect!&body=Great website and portfolio, let's chat!" target="blank"><MdOutlineEmail size={25} className="" /></a>
+            <a style={{ color: 'inherit' }} className='m-1 d-none d-md-inline fs-5' href="https://www.linkedin.com/in/dunnevan/" target="blank">LinkedIn</a>
+            <a style={{ color: 'inherit' }} className='m-1 d-none d-md-inline fs-5'  href="https://github.com/dunndealpro/turtle-game" target="blank">GitHub</a>
+            <a style={{ color: 'inherit' }} className='m-1 d-none d-md-inline fs-5' href="mailto:dunn.evan.f@gmail.com?subject=Cool App!&body=Cool app, let's chat!" target="blank">Contact</a>
           {/* </Col> */}
         </Nav>
         {/* </Navbar.Collapse> */}
